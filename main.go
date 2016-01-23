135ae96c7fb446f7d33f28445cf53fc454e6196e
Masato Ikeda <masato.ikeda@gmail.com>
2016-01-24 00:22:29 +0900

    Read files from STDIN

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
-	args := os.Args[1:]
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	lines := strings.Split(string(bytes.TrimRight(data, "\n")), "\n")
+
+	args0 := os.Args[1:]
+
+	args := append(args0, lines...)
+
 	cmd := exec.Command("open", args...)
 	out, _ := cmd.CombinedOutput()
 	fmt.Println(string(out))
