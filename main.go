9ddffec93c40e7767faf4a0523fc636643c6cc2b
Masato Ikeda <masato.ikeda@gmail.com>
2016-01-24 01:05:01 +0900

    Go through with blank stdin

--- main.go
+++ main.go
@@ -10,19 +10,25 @@ import (
 )
 
 func main() {
-	data, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	lines := strings.Split(string(bytes.TrimRight(data, "\n")), "\n")
-
+	lines := getStdinLines()
 	args0 := os.Args[1:]
-
 	args := append(args0, lines...)
 
 	cmd := exec.Command("open", args...)
 	out, _ := cmd.CombinedOutput()
 	fmt.Println(string(out))
+}
+
+func getStdinLines() []string {
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		data, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		return strings.Split(string(bytes.TrimRight(data, "\n")), "\n")
+	}
+
+	return []string{}
 }
